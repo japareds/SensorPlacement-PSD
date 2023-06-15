382b576b39e13e434eba050e93b577c16f7fbd07
Juan Antonio Paredes Ahumada <52574469+japareds@users.noreply.github.com>
2023-06-15 17:14:04 +0200

    Added rotation experiment
    Added rotations to the eigenbasis that maps the simplex

--- Experiment_PSD_Rotation.py
+++ Experiment_PSD_Rotation.py
@@ -13,6 +13,8 @@ from matplotlib import pyplot as plt
 import warnings
 import decimal
 import scipy.linalg
+import math
+import time
 
 import LoadDataSet as LDS
 import LowRankDecomposition as LRD
@@ -156,7 +158,7 @@ class Basis():
         R = self.rotation_matrix(u,v,theta)
         return R@self.Psi
     
-    def perturbate_eigenmodes(self,mode='rotation',num_rotations = 5,theta_max=np.pi/20):
+    def perturbate_eigenmodes(self,mode='rotation',num_rotations = 5,theta_max=np.pi/20,rotation_vector=0):
         """
         Perturbate eigenmodes to new values.
         """
@@ -173,128 +175,18 @@ class Basis():
         
         elif mode=='rotation_single':
             possible_vectors = np.array([np.array(i).T for i in itertools.combinations(np.identity(self.n),2)])
-            rotation_vector = possible_vectors[0]
+            rotation_vector = possible_vectors[rotation_vector]
             Psi = self.rotation_matrix(rotation_vector[:,0][:,None],rotation_vector[:,1][:,None],theta_max)@self.Psi
             
         self.Psi = Psi
                 
-                
-            
-        
-    
-    
-#%%
-def compare_solution(C,H,Psi,obj,t,S,r,n,p):
+# =============================================================================
+# Mesh
+# =============================================================================
+def Mesh_LMI(t_range,p,n,Psi):
     """
-    Given C,H,obj: solutions of the convex optimization problem at step t
-    Compute and compare covariance matrix
+    LMI evolution at different t-thresholds
     """
-    # real solutions
-    In = np.identity(n)
-    C_sparse = np.array([x for x in itertools.combinations(In,p)])
-    covariances = [C@Psi@np.linalg.inv(Psi.T@C.T@C@Psi)@Psi.T@C.T for C in C_sparse]
-    print('Different covariance matrices')
-    for i in range(C_sparse.shape[0]):
-        print(f'C=\n{C_sparse[i]}\nSigma=\n{covariances[i]}')
-    
-    # SDP solution at step t
-    cov = C@Psi@np.linalg.inv(Psi.T@H@Psi)@Psi.T@C.T
-    print(f'\nLinear solution at threshold t<={t}\nobjective={obj}\nC=\n{C}\nH=\n{H}\nSigma=\n{cov}')
-    print(f'Maximum diagonal value of covariance matrix: {np.diag(cov).max()}')
-    
-    # vertex solutions
-    In = (1/p)*np.identity(n)
-    x_vertex = np.array([np.tile(i,p).reshape((p,n)) for i in itertools.combinations(In,1)])
-    x_vertex = np.array([np.sum(i,axis=0) for i in itertools.combinations(x_vertex,2)])
-    cov_discrete = [C@Psi@np.linalg.inv(Psi.T@np.diag(C.sum(axis=0))@Psi)@Psi.T@C.T for C in x_vertex]
-    
-    print(f'\nVertex solutions:')
-    for i in range(x_vertex.shape[0]):
-        print(f'C=\n{x_vertex[i]}\nH=\n{np.diag(x_vertex[i].sum(axis=0))}\nSigma=\n{cov_discrete[i]}')
-        
-    return
-
-def check_solution_PSD():
-
-    B = np.zeros((r+1,r+1))
-    B[-1,-1] = t
-
-    x = np.tile(C_t[t][0,:],p)
-    LMI_sparse_solution = np.array([x[i]*S[i] for i in range(n*p)]).sum(axis=0)
-    Ip = np.identity(p)
-    R = [np.block([[Psi,np.zeros((n,1))],[np.zeros((p,r)),Ip[:,j][:,None]]]) for j in range(p)]
-    LMI_solution = [R[i].T@LMI_sparse_solution@R[i] + B for i in range(p)]# p LMIs per configuration
-    if all (np.concatenate([np.linalg.eig(LMI)[0] for LMI in LMI_solution])>=0):
-        print(f'PSD matrix\neigenvalues: {np.concatenate([np.linalg.eig(LMI)[0] for LMI in LMI_solution])}')
-    elif any(np.concatenate([np.linalg.eig(LMI)[0] for LMI in LMI_solution])<0):
-        print(f'non-PSD matrix\neigenvalues: {np.concatenate([np.linalg.eig(LMI)[0] for LMI in LMI_solution])}')
-    return 
-    
-
-
-#%% plots
-def plot_all_projections(t_plot,figures,C_t,H_t,obj_t,S):
-    """
-    Plot different polytope projections at a given optimization step t_plot
-    """
-    # linear plots
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='x-linear')
-    
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag')
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag1-mixed')
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag2-mixed')
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag3-mixed')
-    
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag12-mixed')
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag13-mixed')
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag23-mixed')
-    
-    # quadratic plots
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag-squared')
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag1-mixed-squared')
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag2-mixed-squared')
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag3-mixed-squared')
-    
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag12-mixed-squared')
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag13-mixed-squared')
-    figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t_plot,projection='diag23-mixed-squared')
-    
-    return 
-#%% projection in time: both in sparse space and after mapping
-def plot_projection_in_time(figures,plot_range,LMI,LMI_q,C_t,H_t,x_PSD_t,projection='x-linear'):
-    """
-    Visualize  the increase of non-PSD zone for different optimization steps t = plot_range
-    """
-    for t in plot_range:
-        print('\n')
-        t = np.round(t,exponent)
-        print(f'{t}')
-        figures.plot_projection(LMI, LMI_q, H_t, C_t, x_PSD_t, t,projection)
-        figures.plot_projection_Schur(x_PSD_t,B_t,C_t,H_t,t)    
-    
-    
-    return
-
-#%%
-if __name__=='__main__':
-    abs_path = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.abspath(os.path.join(abs_path,os.pardir)) + '/Files/'
-    results_path = os.path.abspath(os.path.join(abs_path,os.pardir)) + '/Results/'
-    # =============================================================================
-    #     Parameters
-    # =============================================================================
-    n,p,r = 3,2,1
-    basis = Basis(n,r,basis='random')
-    basis.create_psi_basis(random_seed=40)
-    Psi_orig = basis.Psi.copy()
-    #basis.perturbate_eigenmodes(mode='rotation_single',num_rotations = 1,theta_max=np.deg2rad(1))
-    Psi = basis.Psi
-    
-    
-    # =============================================================================
-    # Get LMIs
-    # =============================================================================
-    
     # sparse matrices
     S = []
     for i in range(p):
@@ -307,9 +199,7 @@ if __name__=='__main__':
     LMI = []
     LMI,LMI_q,x_valid = get_lmi_sparse(S,n,p,complexity='n')
     
-    t_tol = 1e-3
-    exponent = -1*decimal.Decimal(str(t_tol)).as_tuple().exponent
-    t_range = np.concatenate([np.arange(0.,r/p+t_tol,t_tol),np.arange(r/p + t_tol,2*r/p+t_tol,t_tol)])
+    
     B_t = {np.round(el,exponent):0 for el in t_range}
     for t in t_range:
         t = np.round(t,exponent)
@@ -323,54 +213,143 @@ if __name__=='__main__':
     x_PSD_t = {np.round(el,exponent):0 for el in t_range}
     for t in t_range:
         t = np.round(t,exponent)
-        print(t)
         B = B_t[t]
         x_PSD_t[t] = get_lmi_non_sparse(LMI,x_valid,Psi,n,p,r,B)
-        
-         
-    # =============================================================================
-    # Solve ConvexOpt problem
-    # =============================================================================
-    print(f'Solving convex optimization problem at different t values:\n{[np.round(i,2) for i in t_range]}\nPoint of interest at r/p = {r/p}')
-    H_t={np.round(el,exponent):0 for el in t_range}
-    C_t={np.round(el,exponent):0 for el in t_range}
-    obj_t = {np.round(el,exponent):0 for el in t_range}
     
+    return x_PSD_t
+
+def Convex_LMI(Psi,n,p,r,t):
     convex_opt = SPM.ConvexOpt(Psi,n,p,r)
-    for t in t_range:
-        t = np.round(t,exponent)
-        convex_opt.ConvexOpt_LMI(threshold_t=t,var_beta=1.0)
-        C,H,obj = convex_opt.C, convex_opt.H,convex_opt.obj
-        H_t[t] = H
-        C_t[t] = C
-        obj_t[t] = obj
+    convex_opt.ConvexOpt_LMI(threshold_t=t,var_beta=1.0)
+    C,H,obj = convex_opt.C, convex_opt.H,convex_opt.obj
+    return C,H,obj
+#%%
+def discretization(C,H,p,n):
+    """
+    converts C,H from linear solution with values in [0,1] into discrete {0,1}
+    """
+    locations = np.argsort(np.diag(H))[-p:]
+    In = np.identity(n)
+    C_discrete = In[np.sort(locations)]
+    H_discrete = C_discrete.T@C_discrete
+    return C_discrete, H_discrete
+
+def compare_solution(C,H,Psi,obj,r,n,p,t):
+    """
+    Given C,H,obj: solutions of the convex optimization problem
+    Compute and compare covariance matrix
+    """
+    # real solutions
+    In = np.identity(n)
+    C_sparse = np.array([x for x in itertools.combinations(In,p)])
+    covariances = [C@Psi@np.linalg.inv(Psi.T@C.T@C@Psi)@Psi.T@C.T for C in C_sparse]
+    # print('Different covariance matrices')
+    # for i in range(C_sparse.shape[0]):
+    #     print(f'C=\n{C_sparse[i]}\nSigma=\n{covariances[i]}')
+    print(f'Worst case scenario covariance entry:\nCombination of maximum:\n{C_sparse[np.argmax(([np.diag(cov).max() for cov in covariances]))]}\nValue:{np.max([np.diag(cov).max() for cov in covariances])}\nCombination of minimum:\n{C_sparse[np.argmin(([np.diag(cov).max() for cov in covariances]))]}\nValue: {np.min([np.diag(cov).max() for cov in covariances])}')
+    
+    # SDP solution at step t
+    cov = C@Psi@np.linalg.inv(Psi.T@H@Psi)@Psi.T@C.T
+    print(f'\nLinear solution at threshold t>={t}\nobjective={obj}\nC=\n{C}\nH=\n{H}\nCovariance=\n{cov}')
+    print(f'Maximum diagonal value of covariance matrix: {np.diag(cov).max()}')
+    
+    # SDP solution discretization
+    C_discrete, H_discrete = discretization(C,H,p,n)
+    cov_real = C_discrete@Psi@np.linalg.inv(Psi.T@H_discrete@Psi)@Psi.T@C_discrete.T
+    print(f'Discretization of SDP results:\nLocations:\n{C_discrete}\ncovariance:\n{cov_real}\nMax diagonal value: {np.max(np.diag(cov_real))}')
     
     
-    #check_solution(C_t[t_check],H_t[t_check],Psi,obj_t[t_check],S,r,n,p)
+    # # vertex solutions
+    # In = (1/p)*np.identity(n)
+    # x_vertex = np.array([np.tile(i,p).reshape((p,n)) for i in itertools.combinations(In,1)])
+    # x_vertex = np.array([np.sum(i,axis=0) for i in itertools.combinations(x_vertex,2)])
+    # cov_discrete = [C@Psi@np.linalg.inv(Psi.T@np.diag(C.sum(axis=0))@Psi)@Psi.T@C.T for C in x_vertex]
+    
+    # print(f'\nVertex solutions:')
+    # for i in range(x_vertex.shape[0]):
+    #     print(f'C=\n{x_vertex[i]}\nH=\n{np.diag(x_vertex[i].sum(axis=0))}\nSigma=\n{cov_discrete[i]}')
+        
+    return
+#%%
+if __name__=='__main__':
+    abs_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.abspath(os.path.join(abs_path,os.pardir)) + '/Files/'
+    results_path = os.path.abspath(os.path.join(abs_path,os.pardir)) + '/Results/'
+    # =============================================================================
+    #     Parameters
+    # =============================================================================
+    n,p,r = 30,12,7
+    basis = Basis(n,r,basis='random')
+    basis.create_psi_basis(random_seed=92)
+    Psi_orig = basis.Psi.copy()
+    
+    
+    t_tol = 1e-3
+    t_range = np.arange(0.,r/p+t_tol,t_tol)
+    exponent = -1*decimal.Decimal(str(t_tol)).as_tuple().exponent
     
     # =============================================================================
-    # Plot projections
+    #     Mesh cone
     # =============================================================================
-    figures = Plots.Plots_Projection(n, p, r, Psi)
-    figures.plot_count_non_PSD(LMI,x_PSD_t)
-    # different projections at step t
-    t_plot = -1
-    if t_plot>=0.0:
-        plot_all_projections(t_plot,figures,C_t,H_t,obj_t,S)
     
-    # different
-    
-    plot_range = [0.478,0.477,0.476]# different levels of PSD-ness
-    if len(plot_range)>0:
-        plot_projection_in_time(figures,plot_range,LMI,LMI_q,C_t,H_t,x_PSD_t,projection='x-linear')
-    
-    t_check= 0.475 # random seed 92: 0.476 fails
-    compare_solution(C_t[t_check],H_t[t_check],Psi,obj_t[t_check],t_check,S,r,n,p)
-    
-    # eigenvalues
-    #figures.plot_vertex_eigenvalues(B_t)
-    
+    create_mesh = False
+    if create_mesh:
+        x_PSD_t = Mesh_LMI(t_range,p,n,basis.Psi)
+        t_last_point = r/p
+        for t in np.arange(r/p,0.0,-t_tol):
+            t = np.round(t,exponent)
+            if len(x_PSD_t[t][0]) == 0:
+                break
+            else:
+                t_last_point = t
+    else:
+        t_last_point = 0.0
 
+    # =============================================================================
+    # Find solution point via SDP
+    # =============================================================================
+
+    C_init,H_init,obj_init = Convex_LMI(Psi_orig,n,p,r,0.0)
     
-
-
+    
+    proximity_tol = 4e-2
+    time_init = time.time()
+    n_iter = 500
+    C_iter = {el:0 for el in range(n_iter)}
+    H_iter = {el:0 for el in range(n_iter)}
+    for i in range(n_iter):
+        rng = np.random.default_rng(seed=i)
+        count = 0
+        basis.Psi = Psi_orig
+        vertex_proximity = np.sum(np.diag(H_init)**2)
+        while p-vertex_proximity > proximity_tol:  # still far from vertex
+            count+=1
+            theta,plane = rng.normal(loc=0.0,scale=1.0,size=1)[0],rng.integers(low=0.0,high=math.comb(n,2) -1)
+            basis.perturbate_eigenmodes(mode='rotation_single',theta_max=np.deg2rad(theta),rotation_vector=plane)
+            
+            C,H,obj = Convex_LMI(basis.Psi,n,p,r,0.0)
+            vertex_proximity_iter = np.sum(np.diag(H)**2) # tends to p as the point is close to a vertex
+            
+            if vertex_proximity_iter - vertex_proximity > 1e-4:# keep basis and update proximity
+                vertex_proximity = vertex_proximity_iter
+            else:# keep old proximity and return rotation
+                basis.perturbate_eigenmodes(mode='rotation_single',theta_max=np.deg2rad(-theta),rotation_vector=plane)
+        C_iter[i] = C
+        H_iter[i] = H
+        
+        
+    time_end = time.time()
+    print(f'Finished in: {(time_end-time_init):.2f} seconds after {count} rotations')
+    
+    # Psi_final = basis.Psi
+    # print('\nOriginal basis results')
+    compare_solution(C_init,H_init,Psi_orig,obj_init,r,n,p,t_last_point)
+    # print('\nFinal basis results')
+    # compare_solution(C,H,Psi_final,obj,r,n,p,t_last_point)
+    # print('\nOriginal basis with final locations')
+    # compare_solution(C,H,Psi_orig,obj,r,n,p,t_last_point)
+    
+    
+    
+    
+    
